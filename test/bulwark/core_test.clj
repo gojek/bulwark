4fd7d4680a1c996797a13758486184aa33436dd2
koushik.k <koushik.k@go-jek.com>
2018-11-20 10:45:55 +0530

    Avoid use of promise

--- test/bulwark/core_test.clj
+++ test/bulwark/core_test.clj
@@ -6,51 +6,51 @@
   (testing "Successful call"
     (let [fallback (fn [_]
                      "failure")
-          result   (bulwark/with-hystrix {:group-key                          "foo"
-                                          :command-key                        "foo"
-                                          :thread-pool-key                    "foo"
-                                          :thread-count                       2
-                                          :breaker-sleep-window-ms            500
-                                          :breaker-error-threshold-percentage 20
-                                          :execution-timeout-ms               100
-                                          :fallback-fn                        fallback}
-                     "success")]
+          result (bulwark/with-hystrix {:group-key                          "foo"
+                                        :command-key                        "foo"
+                                        :thread-pool-key                    "foo"
+                                        :thread-count                       2
+                                        :breaker-sleep-window-ms            500
+                                        :breaker-error-threshold-percentage 20
+                                        :execution-timeout-ms               100
+                                        :fallback-fn                        fallback}
+                                       "success")]
       (is (= result "success"))))
 
   (testing "Fallback when exception is thrown"
     (let [failed-exception (promise)
-          fallback         (fn [{:keys [failed-execution-exception]}]
-                             (deliver failed-exception failed-execution-exception)
-                             "failure")
-          result           (bulwark/with-hystrix {:group-key                          "bar"
-                                                  :command-key                        "bar"
-                                                  :thread-pool-key                    "bar"
-                                                  :thread-count                       2
-                                                  :breaker-sleep-window-ms            500
-                                                  :breaker-error-threshold-percentage 20
-                                                  :execution-timeout-ms               100
-                                                  :fallback-fn                        fallback}
-                             (throw (ex-info "foo" {:error "foo"}))
-                             "success")]
+          fallback (fn [{:keys [failed-execution-exception]}]
+                     (deliver failed-exception failed-execution-exception)
+                     "failure")
+          result (bulwark/with-hystrix {:group-key                          "bar"
+                                        :command-key                        "bar"
+                                        :thread-pool-key                    "bar"
+                                        :thread-count                       2
+                                        :breaker-sleep-window-ms            500
+                                        :breaker-error-threshold-percentage 20
+                                        :execution-timeout-ms               100
+                                        :fallback-fn                        fallback}
+                                       (throw (ex-info "foo" {:error "foo"}))
+                                       "success")]
       (is (= result "failure"))
       (is (= (ex-data @failed-exception)
              {:error "foo"}))))
 
   (testing "Fallback when body times out"
     (let [timed-out? (promise)
-          fallback   (fn [{:keys [response-timed-out?]}]
-                       (deliver timed-out? response-timed-out?)
-                       "failure")
-          result     (bulwark/with-hystrix {:group-key                          "goo"
-                                            :command-key                        "goo"
-                                            :thread-pool-key                    "goo"
-                                            :thread-count                       2
-                                            :breaker-sleep-window-ms            500
-                                            :breaker-error-threshold-percentage 20
-                                            :execution-timeout-ms               100
-                                            :fallback-fn                        fallback}
-                       (Thread/sleep 200)
-                       "success")]
+          fallback (fn [{:keys [response-timed-out?]}]
+                     (deliver timed-out? response-timed-out?)
+                     "failure")
+          result (bulwark/with-hystrix {:group-key                          "goo"
+                                        :command-key                        "goo"
+                                        :thread-pool-key                    "goo"
+                                        :thread-count                       2
+                                        :breaker-sleep-window-ms            500
+                                        :breaker-error-threshold-percentage 20
+                                        :execution-timeout-ms               100
+                                        :fallback-fn                        fallback}
+                                       (Thread/sleep 200)
+                                       "success")]
       (is (= result "failure"))
       (is @timed-out?))))
 
@@ -66,14 +66,14 @@
                                               :breaker-error-threshold-percentage 20
                                               :execution-timeout-ms               100
                                               :fallback-fn                        fallback}
-                                             "success")]
-      (is (= @result "success"))))
+                                             {:status "success" :response "success"})]
+      (is (= (:status @result) "success"))
+      (is (= (:response @result) "success"))))
 
   (testing "Fallback when exception is thrown"
-    (let [failed-exception (promise)
-          fallback (fn [{:keys [failed-execution-exception]}]
-                     (deliver failed-exception failed-execution-exception)
-                     "failure")
+    (let [fallback (fn [{:keys [failed-execution-exception]}]
+                     {:status "failure" :response failed-execution-exception}
+                     )
           result (bulwark/with-hystrix-queue {:group-key                          "bar"
                                               :command-key                        "bar"
                                               :thread-pool-key                    "bar"
@@ -84,15 +84,13 @@
                                               :fallback-fn                        fallback}
                                              (throw (ex-info "foo" {:error "foo"}))
                                              "success")]
-      (is (= @result "failure"))
-      (is (= (ex-data @failed-exception)
+      (is (= (:status @result) "failure"))
+      (is (= (ex-data (:response @result))
              {:error "foo"}))))
 
   (testing "Fallback when body times out"
-    (let [timed-out? (promise)
-          fallback (fn [{:keys [response-timed-out?]}]
-                     (deliver timed-out? response-timed-out?)
-                     "failure")
+    (let [fallback (fn [{:keys [response-timed-out?]}]
+                     {:status "failure" :response response-timed-out?})
           result (bulwark/with-hystrix-queue {:group-key                          "goo"
                                               :command-key                        "goo"
                                               :thread-pool-key                    "goo"
@@ -103,5 +101,5 @@
                                               :fallback-fn                        fallback}
                                              (Thread/sleep 200)
                                              "success")]
-      (is (= @result "failure"))
-      (is @timed-out?))))
+      (is (= (:status @result) "failure"))
+      (is (:response @result)))))
