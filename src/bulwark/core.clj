0b8672d40fd4c7e3faf8a195282c3a87dcfe0894
Kartik Gupta <rulz.gupta@gmail.com>
2019-06-03 17:29:31 +0530

    Fixes indentation

--- src/bulwark/core.clj
+++ src/bulwark/core.clj
@@ -21,22 +21,22 @@
   (fn [_ ^HystrixCommand$Setter setter]
     (-> setter
         (.andCommandPropertiesDefaults
-          (-> (HystrixCommandProperties/Setter)
-              (.withCircuitBreakerSleepWindowInMilliseconds breaker-sleep-window-ms)
-              (.withCircuitBreakerErrorThresholdPercentage breaker-error-threshold-percentage)
-              (.withExecutionTimeoutInMilliseconds execution-timeout-ms)))
+         (-> (HystrixCommandProperties/Setter)
+             (.withCircuitBreakerSleepWindowInMilliseconds breaker-sleep-window-ms)
+             (.withCircuitBreakerErrorThresholdPercentage breaker-error-threshold-percentage)
+             (.withExecutionTimeoutInMilliseconds execution-timeout-ms)))
         (.andThreadPoolPropertiesDefaults
-          (.withCoreSize (HystrixThreadPoolProperties/Setter) thread-count)))))
+         (.withCoreSize (HystrixThreadPoolProperties/Setter) thread-count)))))
 
 (defn- fallback-wrapper [fallback-fn]
   (capture-logging-context
-    (fn []
-      (let [^HystrixCommand command hystrix/*command*]
-        (fallback-fn {:response-timed-out?        (.isResponseTimedOut command)
-                      :failed-execution-exception (.getFailedExecutionException command)
-                      :circuit-breaker-open?      (.isCircuitBreakerOpen command)
-                      :semaphore-rejected?        (.isResponseSemaphoreRejected command)
-                      :thread-pool-rejected?      (.isResponseThreadPoolRejected command)})))))
+   (fn []
+     (let [^HystrixCommand command hystrix/*command*]
+       (fallback-fn {:response-timed-out?        (.isResponseTimedOut command)
+                     :failed-execution-exception (.getFailedExecutionException command)
+                     :circuit-breaker-open?      (.isCircuitBreakerOpen command)
+                     :semaphore-rejected?        (.isResponseSemaphoreRejected command)
+                     :thread-pool-rejected?      (.isResponseThreadPoolRejected command)})))))
 
 (defn- hystrix-conf [{:keys [group-key thread-pool-key command-key run-fn fallback-fn]
                       :as config}]
@@ -49,7 +49,7 @@
                                                           :breaker-error-threshold-percentage
                                                           :execution-timeout-ms]))
            :run-fn          (capture-logging-context run-fn)}
-          (some? fallback-fn) (assoc :fallback-fn (fallback-wrapper fallback-fn))))
+    (some? fallback-fn) (assoc :fallback-fn (fallback-wrapper fallback-fn))))
 
 (defn execute-with-hystrix [key-map]
   (let [definition (hystrix-conf key-map)]
